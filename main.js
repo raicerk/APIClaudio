529ebc40bcc29111f706f9ad4768194847b947e7
Juan Mora <raicerk@gmail.com>
2020-03-24 22:48:19 -0300

    se inicia el proyecto de CLaudio

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(201).json({
+      "saludo": "hola mundo cruel"
+  });
+});
+
+app.post('/suma', (req, res)=>{
+    res.status(200).json({
+        "resultado": parseInt(req.body.numero1) + parseInt(req.body.numero2)
+    })
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
