fc61ae6fe7b13527bcd548a114bbf1b1f2e984e0
Juan Mora <raicerk@gmail.com>
2020-03-28 17:48:30 -0300

    Se agrega dato para mapear la respuesta

--- persona.js
+++ persona.js
@@ -3,6 +3,7 @@ exports.listar = arreglo => {
     return arreglo.map((item, i) => {
         return {
             index: i,
+            id: item._id,
             uuid: item.uuid,
             nombre: item.nombre,
             apellido: item.apellido,
