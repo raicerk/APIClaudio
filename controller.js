5cd3be886e28ea9e292ba59c98071397d35b301b
Juan Mora <juanmora.globant@latam.com>
2020-03-29 13:14:20 -0300

    Mejoras menores

--- controller.js
+++ controller.js
@@ -4,41 +4,32 @@ const perso = require('./persona');
 var arreglo = []
 
 exports.add = (req, res) => {
-
-    let contacto = {
+    conn.collection("contactos").insertOne({
         uuid: uuid.v4(),
         nombre: req.body.nombre,
         apellido: req.body.apellido,
         telefono: req.body.telefono
-    }
-
-    conn.collection("contactos").insertOne(contacto, (err, response) =>{
-        if (err) {
-            res.status(400).json({
-                status: err
-            })
-        }else{
-            console.log(response.result);
-            res.status(201).json({
-                status: "registro agregado correctamente"
-            })
-        }
-    });  
+    }).then(resp=>{
+        console.log(resp.result);
+        res.status(201).json({
+            status: "registro agregado correctamente"
+        })
+    })
 }
 
 exports.getAll = (req, res) => {
-    conn.collection("contactos").find({}).toArray((err,response)=>{
+    conn.collection("contactos").find({}).toArray().then(result=>{
         res.status(200).json({
-            personas: perso.listar(response)
+            personas: perso.listar(result)
         })
     });
 }
 
 exports.delete = (req, res) => {
-
     conn.collection("contactos").deleteOne({uuid: req.params.uuid}).then(resp => {
         console.log(resp.result)
     })
+    
     conn.collection("contactos").find({}).toArray().then(result=>{
         res.status(200).json({
             personas: perso.listar(result)
@@ -55,7 +46,6 @@ exports.get = (req, res) => {
 }
 
 exports.update = (req, res) => {
-
     conn.collection("contactos").updateOne({ uuid: req.params.uuid }, { 
         $set: {
             nombre: req.body.nombre,
